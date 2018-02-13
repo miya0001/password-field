b568304679d6327fc4fca58268cb4edbda47dcaf
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-02-14 05:04:52 +0900

    related to documentation

--- src/PasswordField.test.js
+++ src/PasswordField.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import PasswordField from './PasswordField';
 
+// See https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#testing-components
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MuiThemeProvider><PasswordField id="password" /></MuiThemeProvider>, div);
