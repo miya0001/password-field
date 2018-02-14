541807f8f85395a02dfb9e993f0d34260be885a9
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-02-14 14:33:17 +0900

    improve tests

--- src/PasswordField.test.js
+++ src/PasswordField.test.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import PropTypes from 'prop-types';
 import PasswordField from './PasswordField';
 
+const muiTheme = getMuiTheme();
+
 it('renders without crashing', () => {
-  const wrapper = mount(<MuiThemeProvider><PasswordField id="hello" name="world" /></MuiThemeProvider>);
+  const wrapper = mount(<PasswordField id="hello" name="world" />, {
+    context: { muiTheme },
+    childContextTypes: { muiTheme: PropTypes.object },
+  });
+
   expect(wrapper.find("input").length).to.equal(1);
-  // `<PasswordField />` is one of the children for `<MuiThemeProvider />`.
-  // So it needs `.children.props` to get props.
-  expect(wrapper.props().children.props.id).to.equal("hello");
-  expect(wrapper.props().children.props.name).to.equal("world");
+  expect(wrapper.props().id).to.equal("hello");
+  expect(wrapper.props().name).to.equal("world");
+  expect(wrapper.state().visibility).to.equal(false);
+  expect(wrapper.state().inputType).to.equal('password');
 });
